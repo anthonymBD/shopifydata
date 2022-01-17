4ba1d17c30ff546e87fc6aaa0e38fe138683ac14
anthonymBD <97461227+anthonymBD@users.noreply.github.com>
2022-01-17 15:49:59 -0600

    Add files via upload

---
+++ downloader.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import urllib.request
+
+def url_to_jpg(i, url, file_path):
+
+    filename = 'shopify-{}.jpg'.format(i)
+    full_path = '{}{}'.format(file_path, filename)
+    urllib.request.urlretrieve(url, full_path)
+
+    print('{} saved.'.format(filename))
+
+    return None
+
+FILENAME = 'img_urls.csv'
+FILE_PATH = 'images/'
+
+
+urls = pd.read_csv(FILENAME)
+
+for i, url in enumerate(urls.values):
+    url_to_jpg(i,url[0], FILE_PATH)
