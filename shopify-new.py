40ae3d1a46c72c29ad27e8ae35def6f59cbcf3a6
anthonymBD <97461227+anthonymBD@users.noreply.github.com>
2022-01-17 15:49:35 -0600

    Add files via upload

---
+++ shopify-new.py
@@ -0,0 +1,38 @@
+from hashlib import algorithms_available
+import requests
+import json
+import pandas as pd
+
+url = 'https://wokiee-skins.myshopify.com/products.json?limit=250&page=1'
+
+r = requests.get(url)
+
+data = r.json()
+
+product_list = []
+
+for item in data['products']:
+    title = item['title']
+    product_type = item['product_type']
+    for image in item['images']:
+        try:
+          imgesrc = image['src']
+        except:
+          image = 'None'
+    for variant in item['variants']:
+        price = variant['price']
+        available = variant ['available']
+
+        product = {
+            'title': title,
+            'product_type': product_type,
+            'price': price,
+            'image': imgesrc,
+            'available': available
+        }
+        product_list.append(product)
+
+df = pd.DataFrame(product_list)
+
+df.to_csv('testrun.csv')
+print('saved to file')
